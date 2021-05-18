a0d0718788696b4f44796291e3e1b5f7e5af58c3
Reinoptland <reinoptland@gmail.com>
2021-05-18 16:09:28 +0200

    Todo list & setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+// Beginscherm van de applicatie
+// javascript of html bestand?
+// Het ís javascript, maar sommige lijken html syntax
+// JSX
+
+// Hoe maak je van iets een component
+// 1. Begin de HTML gewoon uit te schrijven (maak eerst wat je wil)
+// 2. Als tevreden bent met je pagina kun een component "refactoren", opschonen
+//   - [ ] Kopier een component met alle data erin
+//   - [ ] Zet het in een ander JavaScript bestand
+//   - [ ] Maak een component functie
+//   - [ ] Die component returned JSX
+//   - [ ] Importeer je component functie in App (of waar je hem wil gebruiken)
+//   - [ ] Gebruik je component functie met JSX syntax
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Nature</h2>
+      <img src="https://source.unsplash.com/1600x900/?nature" />
+
+      <h2>Cheese</h2>
+      <img src="https://source.unsplash.com/1600x900/?cheese" />
+
+      <h2>Travel</h2>
+      <img src="https://source.unsplash.com/1600x900/?travel" />
+
+      <h2>Animals</h2>
+      <img src="https://source.unsplash.com/1600x900/?animals" />
     </div>
   );
 }
